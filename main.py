763dff190a1a188e94d3f56a5a0bdf73b4b9bcb3
pilhwanKim <pilhwankim03@gmail.com>
2018-02-13 07:43:01 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from rx import Observable, Observer
+
+
+def push_hello_world(observer):
+    observer.on_next("hello")
+    observer.on_next("world")
+    # observer.on_error("error")
+    observer.on_completed()
+
+
+class PrintOberver(Observer):
+
+    def on_next(self, value):
+        print("Received {0}".format(value))
+
+    def on_completed(self):
+        print("Done!")
+
+    def on_error(self, error):
+        print("Error Occurred: {0}".format(error))
+
+
+class ListOberver(Observer):
+
+    def __init__(self):
+        self.my_list = []
+
+    def on_next(self, value):
+        self.my_list.append(value)
+
+    def on_completed(self):
+        print(self.my_list)
+        print("Done!")
+
+    def on_error(self, error):
+        print("Error Occurred: {0}".format(error))
+
+
+if __name__ == '__main__':
+    source = Observable.create(push_hello_world)
+    source.subscribe(PrintOberver())
+    source.subscribe(ListOberver())
